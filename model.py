587b175caf95895b5054bbe6a182c76f08eaae0f
Jacob <jacob.zweig@gmail.com>
2017-01-28 15:13:48 -0600

    oops log likelihood = - loss

--- model.py
+++ model.py
@@ -24,20 +24,21 @@ features = 2
 #===========================================================================================#
 # Define Model with alpha (exponential activation) & beta (softplus activation) as outputs
 #===========================================================================================#
-#def model_fn(features, targets, mode, params):
-#        return predictions, loss, train_op
 
-
-def weibull_loglikelihood(args):
+# We put a wrapper around the loss function since keras wants args to be (y_true, y_pred)
+# We could alternatively implement this as a lambda layer, but this seemed easier
+def weibull_loss(args):
     def loss(y_true, y_pred):
         a_, b_, y_, u_ = args
-        hazard0 = tf.pow(tf.div(y_ + 1e-35, a_), b_)
+        hazard0 = tf.pow(tf.div(y_ + K.epsilon(), a_), b_)
         hazard1 = tf.pow(tf.div(y_ + 1, a_), b_)
-        return (tf.mul(u_, tf.log(tf.exp(hazard1 - hazard0) - 1.0)) - hazard1)
+        loglikelihood = (tf.mul(u_, tf.log(tf.exp(hazard1 - hazard0) - 1.0)) - hazard1)
+        return -tf.reduce_mean(loglikelihood)
     return loss
 
 def weibull_beta_penalty(b_,location = 10.0, growth=20.0, name=None):
     # Regularization term to keep beta below location
+    # Not using this yet
 
     with tf.name_scope(name):
         scale = growth/location
@@ -52,31 +53,16 @@ x = LSTM(32, activation='relu')(x)
 alpha = Dense(1, activation='softplus')(x)
 beta = Dense(1, activation='softplus')(x)
 
-# # Train model to output the range we want for betas and alphas
-# model_initializer = Model(input_data, [alpha, beta])
-# model_initializer.compile(loss='mae', optimizer='adam')
-# X_temp = np.random.randint(2, size=(5000, timepoints, features))
-# alphas_temp = np.random.randint(10,20,(5000,1))
-# betas_temp = np.random.randint(10,20,(5000,1))
-# model_initializer.fit(X_temp, [alphas_temp, betas_temp], nb_epoch=1)
-
-
-# Implement custom loss in a lambda layer
-# loss_out = Lambda(weibull_loglikelihood, output_shape=(1,), name='WTTE')([alpha, beta, tte, censored])
-# model = Model(input=[input_data, tte, censored], output=[loss_out])
 model = Model(input=[input_data, tte, censored], output=[alpha, beta])
 
-#loss calculation occurs in lambda layer, so use dummy lambda here for loss
-sgd = SGD(lr=0.00001)
-# model.compile(loss={'WTTE': lambda y_true, y_pred: y_pred}, optimizer=sgd)
-model.compile(loss=weibull_loglikelihood([alpha, beta, tte, censored]), optimizer=sgd)
+sgd = SGD(lr=0.0001)
+model.compile(loss=weibull_loss([alpha, beta, tte, censored]), optimizer=sgd)
 
-# weibull_params_func = K.function([input_data], [alpha, beta])
-# loss_func = K.function([input_data, tte, censored], [loss_out])
 
 
 #============================================#
 #  Specify input data (X, TTE, and censd)
+#     This needs a lot of cleaning up
 #============================================#
 
 TTE = np.zeros(shape=(samples, timepoints))
@@ -100,7 +86,6 @@ for sample in range(TTE.shape[0]):
 
 TTE = TTE[:,1].reshape(samples,1)
 censd = np.random.randint(2, size=(samples, 1))
-# censd = np.ones(shape=(samples,timepoints))
 
 # Dummy labels for dummy loss function
 dummy_labels = np.zeros(shape=(samples, 1))
@@ -111,12 +96,14 @@ dummy_labels = np.zeros(shape=(samples, 1))
 #                 Train
 #============================================#
 output = model.predict([X, TTE, censd])
-# loss = loss_func([X, TTE, censd])
+
+model.fit([X, TTE, censd],
+          [dummy_labels, dummy_labels],
+          nb_epoch=5)
+
 # alphas, betas = weibull_params_func([X])
+output = model.predict([X, TTE, censd])
 
 model.fit([X, TTE, censd],
           [dummy_labels, dummy_labels],
           nb_epoch=1)
-
-# alphas, betas = weibull_params_func([X])
-output = model.predict([X, TTE, censd])
